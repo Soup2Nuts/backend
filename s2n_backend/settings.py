f8294105d7be7d2f2c3ff5b10cbb2adb41c30a49
Ryley Sevier <ryleysevier@gmail.com>
2017-11-14 22:05:53 -0500

    +Implemented the Django framework and hooked it up to the local server's database.

--- s2n_backend/settings.py
+++ s2n_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'backend.apps.BackendConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 's2n_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 's2n',
+        'USER': 's2n',
+        'PASSWORD': 'Bananaphone1!',
+        'HOST': '192.168.1.240',
+        'PORT': '3306'
     }
 }
 
@@ -119,3 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES' : [
+        'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE' : 10
+}
