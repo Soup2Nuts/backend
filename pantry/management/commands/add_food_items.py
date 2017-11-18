d51839943da2505548f5a9c7293d0b2bf4d6dff9
katherine-hough <32645020+katherine-hough@users.noreply.github.com>
2017-11-17 23:28:24 -0500

    Added custom django command
    + command add_food_items
        * used to add food items to the database from a text file
       * see add_food_items.py for usage

---
+++ pantry/management/commands/add_food_items.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from pantry.models import *
+
+class Command(BaseCommand):
+    help = 'Adds food item strings from text files'
+
+    def add_arguments(self, parser):
+        parser.add_argument('text_file', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        for text_file in options['text_file']:
+            try:
+                with open(text_file, 'r') as file:
+                    for line in file:
+                        item = FoodItem()
+                        item.name = line.strip()
+                        item.save()
+                self.stdout.write(self.style.SUCCESS('Successfully added food from "%s"' % text_file))
+            except OSError:
+                self.stdout.write(self.style.FAILURE('Failed to open file "%s"' % text_file))
