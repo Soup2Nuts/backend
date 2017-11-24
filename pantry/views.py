ce7f7e495c48f2c29f29135f9cbb13b2af3815fc
katherine-hough <32645020+katherine-hough@users.noreply.github.com>
2017-11-24 10:13:07 -0500

    Made some some utility methods
    Still checking/testing get_all_valid_recipes, but wanted to get these methods out quickly as they are very useful

--- pantry/views.py
+++ pantry/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import viewsets, permissions, views, status
-from .models import *
-from .serializers import *
 from rest_framework.decorators import permission_classes, detail_route, list_route
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework_jwt.utils import jwt_decode_handler
 from django.core.exceptions import PermissionDenied
+from .utils import *
+from .models import *
+from .serializers import *
 
 class FoodItemViewSet(viewsets.ModelViewSet):
     """ ViewSet for viewing and editing Food Item objects """
@@ -47,7 +47,7 @@ class PantryItemViewSet(viewsets.ViewSet):
         This view should return a list of all the pantry items
         for the currently authenticated user.
         """
-        user = getUser(request)
+        user = get_user(request)
         items = user.pantry.all()
         serializer = self.serializer_class(items, many=True)
         return Response(serializer.data)
@@ -56,7 +56,7 @@ class PantryItemViewSet(viewsets.ViewSet):
         food_name = request.data['food_name']
         #Get the foodItem if it exists
         food = get_object_or_404(FoodItem, pk=food_name)
-        user = PantryItemViewSet.getUser(request)
+        user = get_user(request)
         serializer = self.serializer_class(data={'item':food, 'owner':user.pk })
         if serializer.is_valid():
             serializer.save()
@@ -69,7 +69,7 @@ class PantryItemViewSet(viewsets.ViewSet):
         food_name = food_name.split('=', 1)[1]
         food_name = food_name.replace("+", " ")
         food = get_object_or_404(FoodItem, pk=food_name)
-        user = PantryItemViewSet.getUser(request)
+        user = get_user(request)
         items = PantryItem.objects.all().filter(item=food, owner=user.pk)
         if(len(items) <= 0):
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -89,7 +89,7 @@ class FavoriteRecipeViewSet(viewsets.ViewSet):
         This view should return a list of all the favorite recipes
         for the currently authenticated user.
         """
-        user = getUser(request)
+        user = get_user(request)
         items = user.favorites.all()
         serializer = self.serializer_class(items, many=True)
         return Response(serializer.data)
@@ -98,7 +98,7 @@ class FavoriteRecipeViewSet(viewsets.ViewSet):
         recipe_name = request.data['recipe_name']
         #Get the Recipe if it exists
         recipe = get_object_or_404(Recipe, pk=recipe_name)
-        user = FavoriteRecipeViewSet.getUser(request)
+        user = get_user(request)
         new_fav = FavoriteRecipe(recipe=recipe, owner=user)
         if new_fav:
             new_fav.save()
@@ -110,7 +110,7 @@ class FavoriteRecipeViewSet(viewsets.ViewSet):
         recipe_name = recipe_name.split('=', 1)[1]
         recipe_name = recipe_name.replace("+", " ")
         recipe = get_object_or_404(Recipe, pk=recipe_name)
-        user = FavoriteRecipeViewSet.getUser(request)
+        user = get_user(request)
         recipes = FavoriteRecipe.objects.all().filter(recipe=recipe, owner=user.pk)
         if(len(recipes) <= 0):
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -120,13 +120,3 @@ class FavoriteRecipeViewSet(viewsets.ViewSet):
             raise PermissionDenied("You cannot delete another user's favorites.")
         recipe.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-
-#Decode the user from the request's header
-def getUser(request):
-    try:
-        token = request.META['HTTP_AUTHORIZATION']
-        token = token.split(' ', 1)[1]
-        user_id = jwt_decode_handler(token)['user_id']
-        return User.objects.get(pk=user_id)
-    except:
-        raise PermissionDenied("Request has an invalid or expired token")
